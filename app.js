921fd31ba5b2598c9554b64b248c1074f9ce8476
Anirudh Iyer <anirudh.iyer@outlook.com>
2021-12-30 21:57:49 -0800

    data through imdb, data.js

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-var randomMovieArray = ['Star Wars', 'Game of Thrones', 'Harry Potter', 'Lord of the Rings' ]
-
-var randNumber = Math.floor((Math.random() * randomMovieArray.length) - 1)
-
-var randomMovie = randomMovieArray[randNumber];
-
+// var randomMovieArray = ['The Godfather', 'The Shawshank Redemption', 'Schindler\'s List', 'Raging Bull', 'Casablanca', 'Citizen Kane', '']
 var apikey =  '2bda4263' //not secret, open api
 
 function apiCall() {
-    $.getJSON('http://www.omdbapi.com/?t=' + encodeURI(randomMovie) + "&apikey="+ encodeURI(apikey)).then(function(response) {
-        var image = response.Poster;
+    var randNumber = Math.floor((Math.random() * movies.length))
+    var randomMovie = movies[randNumber].title;
+    console.log(randomMovie)
+    $.getJSON('http://www.omdbapi.com/?t=' + encodeURI(randomMovie) + "&type=movie" + "&apikey="+ encodeURI(apikey)).then(function(response) {
+    var image = response.Poster;
+    var url = "https://www.imdb.com/title/" + response.imdbID;
         if (image !== 'N/A') {
             $('img').attr('src', image);
+            $('a').attr('href', url)
         }
-
-        document.getElementById('movieName').innerText = response.Title;
+        document.getElementById('movieName').innerText = randomMovie;
     })
 }
-
-// apiCall();
+document.getElementById('clicked').addEventListener("click", apiCall)
