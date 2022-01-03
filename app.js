701468f4d9633a9b11eb9c5344623f94b4705e0e
Anirudh Iyer <anirudh.iyer@outlook.com>
2022-01-02 20:13:44 -0800

    test github secret

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 // var randomMovieArray = ['The Godfather', 'The Shawshank Redemption', 'Schindler\'s List', 'Raging Bull', 'Casablanca', 'Citizen Kane', '']
-var apikey =  '2bda4263' //not secret, open api
 
 function apiCall() {
     var randNumber = Math.floor((Math.random() * movies.length))
     var randomMovie = movies[randNumber].title;
     console.log(randomMovie)
-    $.getJSON('https://www.omdbapi.com/?t=' + encodeURI(randomMovie) + "&type=movie" + "&apikey="+ encodeURI(apikey)).then(function(response) {
+    $.getJSON('https://www.omdbapi.com/?t=' + encodeURI(randomMovie) + "&type=movie" + "&apikey="+ encodeURI(APIKEY)).then(function(response) {
     var defaultimg = 'assets/No-Photo-Available.jpg'
     var image = response.Poster;
     console.log(response)
